2ec02c72121eba646aaa91c1887a508210281698
Alan Thomas <alanthomas171@gmail.com>
2026-02-19 16:54:31 +0000

    Add script prop and license dropdown menu

--- main.py
+++ main.py
@@ -2,4 +2,25 @@ from InquirerPy import prompt
 from rich.console import Console
 from rich.table import Table
 from rich.progress import Progress
+from InquirerPy import prompt
+from InquirerPy.base.control import Choice
 import time
+
+
+questions = [
+    {"type": "input", "name": "name", "message": "Enter Project Title?"},
+    {"type": "input", "name": "color", "message": "Enter Project Description"},
+    {"type": "input", "name": "color", "message": "Enter Project Installation Instructions"},
+    {"type": "input", "name": "color", "message": "Enter Project Usage Information"},
+    {
+            "type": "list",
+            "name": "license",
+            "message": "Select an action:",
+            "choices": ["Unlicense", "MIT License", "Apache License 2.0", "BSD 2-Clause ", "BSD 3-Clause ", "ISC License", "GNU GPLv3"],
+            "default": "Unlicense",
+        },
+
+    {"type": "input", "name": "color", "message": "Enter Author Name"},
+    {"type": "input", "name": "color", "message": "Enter Contact Information"},
+]
+answers = prompt(questions)
